e4766c5b9dd4c2a1767594d23c63a74a13a60aa1
kryptoholik1 <kryptoholik@gmail.com>
2019-09-22 18:45:47 +0200

    Gotowa pierwsza lekcja gotowa, wiek mie , picie alkoholu

--- Hello1/Program.cs
+++ Hello1/Program.cs
@@ -7,14 +7,87 @@ namespace Hello1
         static void Main(string[] args)
 
         {
-            //string name = args[0];
-            //Console.WriteLine("witaj świecie," + name);
-
-            foreach (var item in args)
+            ValueTypes();
+            for (; ; )
             {
-                Console.WriteLine("witaj, "+item);
+                Greeting();
+                Age();
+                Settings();
+
+                //foreach (var item in args)
+                //{
+                //    Console.WriteLine("witaj, "+item);
+                //}
+
             }
-           
+        }
+
+        private static void ValueTypes()
+        {
+           int maxInt = int.MaxValue;
+           int minInt = int.MinValue;
+           long maxLong = long.MaxValue;
+           long minLong = long.MinValue;
+
+           Console.WriteLine("maxInt = " + maxInt);
+           Console.WriteLine("minInt = " + minInt);
+           Console.WriteLine("maxLong = " + maxLong);
+           Console.WriteLine("minLong = " + minLong);
+
+        }
+
+
+        /// <summary>
+        /// Ustawienia programu
+        /// </summary>
+        private static void Settings()
+        {
+            Console.ReadKey();
+            Console.Clear();
+            Console.ResetColor();
+        }
+
+        /// <summary>
+        /// wypisujemy komunikat zlażny od wieku
+        /// </summary>
+        private static void Age()
+        {
+            Console.Write("wpisz swój wiek: ");
+            int age;
+            bool result = int.TryParse(Console.ReadLine(), out age);
+
+            if (age > 18)
+
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("brawo . jestes pełnoletnie, możesz pić alkohol");
+
+            }
+            else if (result == false)
+            {
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.WriteLine("wprowadziłeś niepoprawny wiek!");
+
+            }
+
+
+
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("niestety jesteś dzieckiem, pijesz mleko");
+            }
+        }
+
+        /// <summary>
+        /// wypisujemy powietanie użytkownika
+        /// </summary>
+        private static void Greeting()
+        {
+            Console.Write("wpisz swoje imię: ");
+            string name = Console.ReadLine();
+
+            Console.WriteLine("witaj ," + name);
         }
     }
 }
